c7a57fbd2515c7aab18aa9e4b304004dcd370f5f
patronpenguin <xh5277@gmail.com>
2014-02-13 14:50:44 -0500

    Homework 1

---
+++ Documents/workspace/LinesOfAction/src/client/CheatLogic.java
@@ -0,0 +1,11 @@
+package client;
+
+import client.GameApi.VerifyMove;
+import client.GameApi.VerifyMoveDone;
+
+public class CheatLogic {
+	public VerifyMoveDone verify(VerifyMove verifyMove) {
+	    // TODO: I will implement this method in HW2
+	    return new VerifyMoveDone();
+	  }
+}
